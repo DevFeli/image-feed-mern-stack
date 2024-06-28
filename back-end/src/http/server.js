df96e7a55db40412ae5d8631bfe9b14e0d1c94db
Felipe Reis | Acreditar Banco <dev.felipereis@gmail.com>
2024-06-28 09:05:28 -0300

    created .env

--- back-end/src/http/server.js
+++ back-end/src/http/server.js
@@ -1,12 +1,15 @@
+import 'dotenv/config'
 import express from 'express'
 import path from 'path'
 import cors from 'cors'
+
+const PORT = process.env.PORT
 
 const app = express()
 
 app.use(express.json())
 app.use(express.urlencoded({extended: false}))
 
-app.listen(3000, () =>{
-    console.log("Server started.")
+app.listen(PORT, () =>{
+    console.log("Server started on port " + PORT)
 })
