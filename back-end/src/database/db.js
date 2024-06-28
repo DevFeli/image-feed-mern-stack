d930f5601fb6d9232788555c74731f22d5ddf475
Felipe Reis | Acreditar Banco <dev.felipereis@gmail.com>
2024-06-28 10:27:28 -0300

    connection mongo db

---
+++ back-end/src/database/db.js
@@ -0,0 +1,20 @@
+import mongoose from 'mongoose'
+const dbUser = process.env.DB_USER
+const dbPassword = process.env.DB_PASS
+
+const conn = async () =>{
+    try{
+
+        // const dbconn = await mongoose.connect(`mongodb:${dbUser}@${dbPassword}//localhost:27017/`)
+
+        const dbconn = await mongoose.connect(`mongodb://localhost:27017/`)
+
+        return dbconn
+    }catch(e){
+        console.log(e)
+    }
+}
+
+conn()
+
+export default conn
--- back-end/src/http/server.js
+++ back-end/src/http/server.js
@@ -1,7 +1,10 @@
 import 'dotenv/config'
 import express from 'express'
 import path from 'path'
+import { fileURLToPath } from 'url'
 import cors from 'cors'
+
+import '../database/db.js'
 
 import router from '../routes/router.js'
 
@@ -9,9 +12,23 @@ const PORT = process.env.PORT
 
 const app = express()
 
+//resolve paths
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename)
+
+//config data entries
 app.use(express.json())
 app.use(express.urlencoded({extended: false}))
 
+//config cors
+app.use(cors({credentials: true, origin:"http://localhost:3000"}))
+
+//statics
+//uploads images
+app.use("/uploads", express.static(path.resolve(__dirname, '..', 'public', 'uploads')))
+
+
+//routes
 app.use(router)
 
 app.listen(PORT, () =>{
