0efb55b850487dc75afed86d2808426a0095285a
Felipe Reis | Acreditar Banco <dev.felipereis@gmail.com>
2024-06-28 12:49:42 -0300

    create middleware erros validation

--- back-end/src/routes/userRouter.js
+++ back-end/src/routes/userRouter.js
@@ -1,9 +1,11 @@
 import express from 'express'
 import { register } from "../controllers/UserController.js";
 
+import handleValidations from "../middlewares/handleValidations.js"
+
 const userRouter = express.Router()
 
-userRouter.post('/register', register)
+userRouter.post('/register', handleValidations, register)
 
 export default userRouter
 
