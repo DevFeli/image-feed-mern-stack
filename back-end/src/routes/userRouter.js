ba285f94e66a4c449b77fd74de27d713374bdf83
Felipe Reis | Acreditar Banco <dev.felipereis@gmail.com>
2024-06-28 13:05:30 -0300

    validate fields in route register

--- back-end/src/routes/userRouter.js
+++ back-end/src/routes/userRouter.js
@@ -1,11 +1,13 @@
 import express from 'express'
 import { register } from "../controllers/UserController.js";
 
+//middlewares
 import handleValidations from "../middlewares/handleValidations.js"
+import { userCreateValidation } from '../middlewares/userValidations.js'
 
 const userRouter = express.Router()
 
-userRouter.post('/register', handleValidations, register)
+userRouter.post('/register', userCreateValidation(), handleValidations, register)
 
 export default userRouter
 
