6d1e20bf0a04a7c8e12535ad1487f2edb69c764f
Felipe Reis | Acreditar Banco <dev.felipereis@gmail.com>
2024-07-02 15:18:41 -0300

    middleware guard token jwt

---
+++ back-end/src/middlewares/authGuard.js
@@ -0,0 +1,26 @@
+import User from "../models/User";
+import jwt  from 'jsonwebtoken';
+
+const jwtSecret = process.env.SECRET
+
+const authGuard = async (req, res) => {
+
+    const authHeader = req.headers["authorization"]
+    const token = authHeader && authHeader.split(" ")[1]
+
+    if(!token){
+        return res.status(401).json({errors: ["Acesso negado!"]})
+    }
+
+    try{
+
+        const verified = jwt.verify(token, jwtSecret) 
+
+        req.user = await User.findById(verified.id).select("-password")
+
+        next()
+
+    }catch(e){
+        res.status(401).json({errors:["Token inválido."]})
+    }
+}
